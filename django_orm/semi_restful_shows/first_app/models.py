9ef50f755475504f37f178fad32fdabd2cea1e2b
unknown <tamuramark1021@gmail.com>
2020-03-17 21:02:45 -0600

    semi restful shows

--- django_orm/semi_restful_shows/first_app/models.py
+++ django_orm/semi_restful_shows/first_app/models.py
@@ -1,14 +1,90 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.contrib import messages
+import datetime
 
 
+
+    #networks
+
+class TVshow_manager(models.Model):
+    def basic_validator(self, postData):
+        now=datetime.datetime.now()
+        today8am=now.replace(hour=8,minute=0, second=0, microsecond=0)
+        # now== today8am
+        #     False 
+        # now>today8am
+        #     False 
+        errors={}
+        release_date_input=input('dd/mm/yyyy')
+        day,month,year=release_date_input.split('/')
+        isValidDate=True
+        try:
+            datetime.datetime(int(year),int(month),int(day))
+        except ValueError:
+            isValidDate=False
+        if(isValidDate):
+            print('input date is valid')
+        else:
+            print("input date is not valid")    
+        # EMAIL_REGEX=re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        # if not EMAIL_REGEX.match(postDATA['email']):
+            # errors['email']= "invalid email address!"        
+        if len(postData['title'])<1:
+            errors['title']="Title name must have at least 2 characters"
+        if len(postData['network'])<2:
+            errors['network']= "Network name must have at least 3 characters"
+        if now<today8am:
+            True
+        else:
+            False
+            # errors['release_date']="Release date musst be a past date")
+        if len(postData['desc'])<9:
+            errors['desc']="Description should have at least 10 characters"
+        return errors
+        
 class TVshow(models.Model):
     #ID
+    def basic_validator(self, postData):
+        now=datetime.datetime.now()
+        today8am=now.replace(hour=8,minute=0, second=0, microsecond=0)
+        # now== today8am
+        #     False 
+        # now>today8am
+        #     False 
+        errors={}
+        release_date_input=input('dd/mm/yyyy')
+        day,month,year=release_date_input.split('/')
+        isValidDate=True
+        try:
+            datetime.datetime(int(year),int(month),int(day))
+        except ValueError:
+            isValidDate=False
+        if(isValidDate):
+            print('input date is valid')
+        else:
+            print("input date is not valid")    
+        # EMAIL_REGEX=re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        # if not EMAIL_REGEX.match(postDATA['email']):
+            # errors['email']= "invalid email address!"        
+        if len(postData['title'])<1:
+            errors['title']="Title name must have at least 2 characters"
+        if len(postData['network'])<2:
+            errors['network']= "Network name must have at least 3 characters"
+        if now<today8am:
+            True
+        else:
+            False
+            # errors['release_date']="Release date musst be a past date")
+        if len(postData['desc'])<9:
+            errors['desc']="Description should have at least 10 characters"
+        return errors   
     title= models.CharField(max_length=255)
     network=models.CharField(max_length=255)
     release_date=models.DateField()
     desc = models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+    objects=TVshow_manager()
     def __repr__(self):
         return f"<TVshow object: {self.title}({self.id})>"
-    #networks
--- django_orm/semi_restful_shows/first_app/views.py
+++ django_orm/semi_restful_shows/first_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import TVshow
-
+from .models import TVshow, TVshow_manager
+from django.contrib import messages
+import re
 # Create your views here.
 def home(request):
     context={
@@ -13,9 +14,10 @@ def tvshow_new(request):
     return render(request, 'tvshow_new.html')
 
 def add_tvshow(request):
+    
     # some_tvshow=TVshow.objects.get(id=id)
-    # if request.POST['title'] and request.POST['network'] and request.POST['release_date'] and request.POST['desc']:
-    newShow=TVshow.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], desc=request.POST['desc'])
+    if request.POST['title'] and request.POST['network'] and request.POST['release_date'] and request.POST['desc']:
+        newShow=TVshow.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], desc=request.POST['desc'])
     return redirect(f'/tvshow_info/{newShow.id}')
 # may have to add back the network portion of the request.POST['network']
 def tvshow_info(request, id):
@@ -32,19 +34,27 @@ def edit_tvshow(request, id):
     return render(request, 'edit_tvshow.html', context)
 
 def change_tvshow(request, id):
-    changing_tvshow=TVshow.objects.get(id=id)
-    if request.POST['title']:
-        changing_tvshow.title=request.POST['title']
-        changing_tvshow.save()
-    if request.POST['network']:
-        changing_tvshow.network=request.POST['network']
-        changing_tvshow.save()
-    if request.POST['release_date']:
-        changing_tvshow.release_date=request.POST['release_date']
-        changing_tvshow.save()
-    if request.POST['desc']:
-        changing_tvshow.desc=request.POST['desc']
-        changing_tvshow.save()
+    errors= TVshow.objects.basic_validator(request.POST)
+
+    if len(errors)>0:
+        for key,value in errors.items():
+            messages.error(request, value)
+        return redirect(request, 'edit_tvshow.html')
+
+    else:
+        changing_tvshow=TVshow.objects.get(id=id)
+        if request.POST['title']:
+            changing_tvshow.title=request.POST['title']
+            # changing_tvshow.save()
+        if request.POST['network']:
+            changing_tvshow.network=request.POST['network']
+            # changing_tvshow.save()
+        if request.POST['release_date']:
+            changing_tvshow.release_date=request.POST['release_date']
+            # changing_tvshow.save()
+        if request.POST['desc']:
+            changing_tvshow.desc=request.POST['desc']
+            changing_tvshow.save()
     return redirect(f'/tvshow_info/{changing_tvshow.id}')
 
 def delete_tvshow(request, id):
